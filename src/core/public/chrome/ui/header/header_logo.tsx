596d04e83dc23a9a50eeeca47ecc0dd086762b63
Lin Wang <wonglam@amazon.com>
2022-11-07 20:37:26 +0800

    update src/core/public/chrome/ui/header/header_logo.tsx

--- src/core/public/chrome/ui/header/header_logo.tsx
+++ src/core/public/chrome/ui/header/header_logo.tsx
@@ -33,7 +33,6 @@ import { i18n } from '@osd/i18n';
 import React from 'react';
 import useObservable from 'react-use/lib/useObservable';
 import { Observable } from 'rxjs';
-import Url from 'url';
 import { ChromeNavLink } from '../..';
 import { ChromeBranding } from '../../chrome_service';
 
@@ -74,13 +73,14 @@ function onClick(
   }
 
   if (forceNavigation) {
-    const toParsed = Url.parse(anchor.href);
-    const fromParsed = Url.parse(document.location.href);
+    const toParsed = new URL(anchor.href);
+    const fromParsed = new URL(document.location.href);
     const sameProto = toParsed.protocol === fromParsed.protocol;
     const sameHost = toParsed.host === fromParsed.host;
-    const samePath = toParsed.path === fromParsed.path;
+    const samePath = toParsed.pathname === fromParsed.pathname;
+    const sameSearch = toParsed.search === fromParsed.search;
 
-    if (sameProto && sameHost && samePath) {
+    if (sameProto && sameHost && samePath && sameSearch) {
       if (toParsed.hash) {
         document.location.reload();
       }
