855c3a8bfb2124a30684939e2b58128e912d62b0
SuZhou-Joe <suzhou@amazon.com>
2023-09-22 12:30:25 +0800

    Patch/first pr (#194)
    * temp: add unit test
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: add function test for workspace CRUD routes
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: use saved objects client instead of internal repository
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: update CHANGELOG
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: exclude permission check wrapper
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: add integration test
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: add configuration
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: enable workspace flag when run workspace related test
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    ---------
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/plugins/workspace/server/workspace_client.ts
+++ src/plugins/workspace/server/workspace_client.ts
@@ -8,12 +8,13 @@ import type {
   SavedObjectsClientContract,
   CoreSetup,
   WorkspaceAttribute,
-  ISavedObjectsRepository,
+  SavedObjectsServiceStart,
 } from '../../../core/server';
 import { WORKSPACE_TYPE } from '../../../core/server';
 import { IWorkspaceDBImpl, WorkspaceFindOptions, IResponse, IRequestDetail } from './types';
 import { workspace } from './saved_objects';
 import { generateRandomId } from './utils';
+import { WORKSPACE_SAVED_OBJECTS_CLIENT_WRAPPER_ID } from '../common/constants';
 
 const WORKSPACE_ID_SIZE = 6;
 
@@ -23,15 +24,20 @@ const DUPLICATE_WORKSPACE_NAME_ERROR = i18n.translate('workspace.duplicate.name.
 
 export class WorkspaceClientWithSavedObject implements IWorkspaceDBImpl {
   private setupDep: CoreSetup;
-
-  private internalSavedObjectsRepository?: ISavedObjectsRepository;
-  setInternalRepository(repository: ISavedObjectsRepository) {
-    this.internalSavedObjectsRepository = repository;
-  }
+  private savedObjects?: SavedObjectsServiceStart;
 
   constructor(core: CoreSetup) {
     this.setupDep = core;
   }
+
+  private getScopedClientWithoutPermission(
+    requestDetail: IRequestDetail
+  ): SavedObjectsClientContract | undefined {
+    return this.savedObjects?.getScopedClient(requestDetail.request, {
+      excludedWrappers: [WORKSPACE_SAVED_OBJECTS_CLIENT_WRAPPER_ID],
+    });
+  }
+
   private getSavedObjectClientsFromRequestDetail(
     requestDetail: IRequestDetail
   ): SavedObjectsClientContract {
@@ -63,11 +69,13 @@ export class WorkspaceClientWithSavedObject implements IWorkspaceDBImpl {
       const attributes = payload;
       const id = generateRandomId(WORKSPACE_ID_SIZE);
       const client = this.getSavedObjectClientsFromRequestDetail(requestDetail);
-      const existingWorkspaceRes = await this.internalSavedObjectsRepository?.find({
-        type: WORKSPACE_TYPE,
-        search: attributes.name,
-        searchFields: ['name'],
-      });
+      const existingWorkspaceRes = await this.getScopedClientWithoutPermission(requestDetail)?.find(
+        {
+          type: WORKSPACE_TYPE,
+          search: attributes.name,
+          searchFields: ['name'],
+        }
+      );
       if (existingWorkspaceRes && existingWorkspaceRes.total > 0) {
         throw new Error(DUPLICATE_WORKSPACE_NAME_ERROR);
       }
@@ -148,7 +156,9 @@ export class WorkspaceClientWithSavedObject implements IWorkspaceDBImpl {
       const client = this.getSavedObjectClientsFromRequestDetail(requestDetail);
       const workspaceInDB: SavedObject<WorkspaceAttribute> = await client.get(WORKSPACE_TYPE, id);
       if (workspaceInDB.attributes.name !== attributes.name) {
-        const existingWorkspaceRes = await this.internalSavedObjectsRepository?.find({
+        const existingWorkspaceRes = await this.getScopedClientWithoutPermission(
+          requestDetail
+        )?.find({
           type: WORKSPACE_TYPE,
           search: attributes.name,
           searchFields: ['name'],
@@ -183,6 +193,9 @@ export class WorkspaceClientWithSavedObject implements IWorkspaceDBImpl {
         error: this.formatError(e),
       };
     }
+  }
+  public setSavedObjects(savedObjects: SavedObjectsServiceStart) {
+    this.savedObjects = savedObjects;
   }
   public async destroy(): Promise<IResponse<boolean>> {
     return {
