43e91faec7ab11131207f598668b073f727ef073
SuZhou-Joe <suzhou@amazon.com>
2023-10-12 17:45:28 +0800

    [Workspace]Add workspace id in basePath (#212)
    * feat: enable workspace id in basePath
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: add unit test
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: remove useless test object id
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: add unit test
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: add unit test
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: update snapshot
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: move formatUrlWithWorkspaceId to core/public/utils
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: remove useless variable
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: remove useless variable
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: optimization
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: optimization
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: optimization
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: move workspace/utils to core
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: move workspace/utils to core
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: update comment
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: optimize code
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: update unit test
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: optimization
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: add space under license
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * fix: unit test
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    ---------
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/plugins/workspace/server/plugin.ts
+++ src/plugins/workspace/server/plugin.ts
@@ -2,6 +2,7 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
+
 import {
   PluginInitializerContext,
   CoreSetup,
@@ -12,10 +13,27 @@ import {
 import { IWorkspaceDBImpl } from './types';
 import { WorkspaceClientWithSavedObject } from './workspace_client';
 import { registerRoutes } from './routes';
+import { cleanWorkspaceId, getWorkspaceIdFromUrl } from '../../../core/server/utils';
 
 export class WorkspacePlugin implements Plugin<{}, {}> {
   private readonly logger: Logger;
   private client?: IWorkspaceDBImpl;
+
+  private proxyWorkspaceTrafficToRealHandler(setupDeps: CoreSetup) {
+    /**
+     * Proxy all {basePath}/w/{workspaceId}{osdPath*} paths to {basePath}{osdPath*}
+     */
+    setupDeps.http.registerOnPreRouting(async (request, response, toolkit) => {
+      const workspaceId = getWorkspaceIdFromUrl(request.url.toString());
+
+      if (workspaceId) {
+        const requestUrl = new URL(request.url.toString());
+        requestUrl.pathname = cleanWorkspaceId(requestUrl.pathname);
+        return toolkit.rewriteUrl(requestUrl.toString());
+      }
+      return toolkit.next();
+    });
+  }
 
   constructor(initializerContext: PluginInitializerContext) {
     this.logger = initializerContext.logger.get('plugins', 'workspace');
@@ -27,6 +45,8 @@ export class WorkspacePlugin implements Plugin<{}, {}> {
     this.client = new WorkspaceClientWithSavedObject(core);
 
     await this.client.setup(core);
+
+    this.proxyWorkspaceTrafficToRealHandler(core);
 
     registerRoutes({
       http: core.http,
